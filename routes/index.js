5e55864078d87d10fbcf2999e95b56289305da35
Marie Pelizza <marie.pelizza@gmail.com>
2020-04-30 15:08:19 +0200

    page login

--- routes/index.js
+++ routes/index.js
@@ -42,6 +42,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+/* GET login. */
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'Express' });
+});
 
 // Remplissage de la base de donnée, une fois suffit
 router.get('/save', async function(req, res, next) {
